4e383379bbaf622be24b75a81a96afe0e1ecddaf
Red Hendery <red.hendery@gmail.com>
2015-11-03 16:00:46 +1300

    working sign in w/Twitter and sign out methods

--- blog/app/controllers/users/sessions_controller.rb
+++ blog/app/controllers/users/sessions_controller.rb
@@ -1,9 +1,23 @@
 class Users::SessionsController < Devise::SessionsController
 # before_filter :configure_sign_in_params, only: [:create]
 
+@user = User.current_user
+
   # GET /resource/sign_in
-  def new
-    super
+  # def new
+  #   super
+  # end
+
+  def create
+    @user = User.find_or_create_from_auth_hash(auth_hash)
+    self.current_user = @user
+    redirect_to '/'
+  end
+
+  # protected
+
+  def auth_hash
+    request.env['omniauth.auth']
   end
 
   # POST /resource/sign_in
@@ -12,9 +26,10 @@ class Users::SessionsController < Devise::SessionsController
   # end
 
   # DELETE /resource/sign_out
-  # def destroy
-  #   super
-  # end
+  def destroy
+    sign_out @user
+    redirect_to '/'
+  end
 
   # protected
 
